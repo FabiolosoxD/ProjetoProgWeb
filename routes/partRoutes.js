c05f0e56204a0ed2137471818561d40a7f50eb36
Rafael Lourenço <148371020+Rafa200200@users.noreply.github.com>
2024-06-06 23:05:09 +0100

    Final Sprint

--- routes/partRoutes.js
+++ routes/partRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const Part = require('../Models/part');
+const auth = require('../middleware/auth');
 
 // Adicionar uma nova peça
-router.post('/', async (req, res) => {
+router.post('/', auth, async (req, res) => {
   try {
     const { name, type, quantity } = req.body;
-    const part = new Part({ name, type, quantity });
+    const part = new Part({ name, type, quantity, userId: req.user.id });
     await part.save();
     res.status(201).json(part);
   } catch (err) {
@@ -15,7 +16,7 @@ router.post('/', async (req, res) => {
 });
 
 // Listar todas as peças
-router.get('/', async (req, res) => {
+router.get('/', auth, async (req, res) => {
   try {
     const parts = await Part.find();
     res.status(200).json(parts);
@@ -25,7 +26,7 @@ router.get('/', async (req, res) => {
 });
 
 // Atualizar uma peça
-router.patch('/:id', async (req, res) => {
+router.patch('/:id', auth, async (req, res) => {
   try {
     const { id } = req.params;
     const { name, type, quantity } = req.body;
@@ -40,7 +41,7 @@ router.patch('/:id', async (req, res) => {
 });
 
 // Deletar uma peça
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', auth, async (req, res) => {
   try {
     const { id } = req.params;
     const part = await Part.findByIdAndDelete(id);
