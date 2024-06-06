18eac975bc1fb5544b17d24b1fb0b34acc80b17e
Rafael Lourenço <148371020+Rafa200200@users.noreply.github.com>
2024-06-06 23:08:29 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://fabioricomaio:hBQoKUNH3VGBAWQ1@cluster0.3wnprx9.mongodb.net/');
+    await mongoose.connect('mongodb://localhost:27017');
     console.log('MongoDB connected...');
   } catch (err) {
     console.error(err.message);
